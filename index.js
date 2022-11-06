e45788d3adfa2c40e935f1b73e5cc70b7fc98f11
reecco <fredcardoso01@hotmail.com>
2022-11-06 15:38:44 -0300

    fix: error cors

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import router from './src/routes.js'
 
 const app = express()
 const port = process.env.PORT || 9595
 
+app.use(express.json())
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  app.use(cors())
+  next()
+})
+
+app.use('/', router)
+
 dotenv.config()
 
 app.listen(port, () => {
   console.log(`Server open in \nhttp://localhost:${port}`)
 })
-
-app.use(express.json())
-
-app.use('/', router)
