3fde3c2ea8b5a67cde4b074092f3b379b7c76dea
reecco <fredcardoso01@hotmail.com>
2022-12-12 12:50:18 -0300

    fix: error cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 9595
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", 'https://vercel.app')
+  res.header("Access-Control-Allow-Origin", 'https://reecco.vercel.app')
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With")
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST')
   app.use(cors())
