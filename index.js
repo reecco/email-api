d95d8b6728ce235d3ed5047d75704089b17c27cf
reecco <fredcardoso01@hotmail.com>
2022-12-12 12:45:59 -0300

    fix: error cors

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.use(express.json())
 
 app.use((req, res, next) => {
 
-  res.header("Access-Control-Allow-Origin", 'https://reecco-djg1r0fw8-fredericorecco.vercel.app/')
+  const corsOption = {
+    origin: ['https://reecco-djg1r0fw8-fredericorecco.vercel.app/', 'https://reecco-fredericorecco.vercel.app/', 'https://reecco.vercel.app/'],
+  }
+
+  res.header("Access-Control-Allow-Origin", corsOption)
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With")
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST')
   app.use(cors())
